b4c3b2110e74b256f01cc982c468fbe189ec4512
Nina Laribi <ninalaribi@Bilawals-MacBook-Pro.local>
2018-03-04 18:19:54 +0100

    Welcome page primary design

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import './Welcome.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "./Welcome.css";
+import { Link } from "react-router-dom";
 
 class Welcome extends Component {
   render() {
     return (
-      <div className="Welcome">
-        <p>
-            Welcome to the dinner planner React Startup code!
-        </p>
-        
-        <Link to="/search">
-            <button>Start planning</button>
-        </Link>
+      <div className="container-fluid">
+        <div className="row justify-content-md-center">
+          <div className="col-6">
+            <div className="WelcomeScreen">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+
+              <Link to="/search">
+                <button className="btn btn-primary">Start Planning</button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
